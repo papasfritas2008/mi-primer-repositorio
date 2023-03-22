f54494112a91d9c9ce52592590eb4b77e8198705
papasfritas2008 <128107695+papasfritas2008@users.noreply.github.com>
2023-03-22 14:46:32 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# mi-primer-repositorio
-primera vez usando esto
+# PROC11_Plantilla_Proyecto_V4
--- sketch.js
+++ sketch.js
@@ -1,2 +1,26 @@
-console.log("hola");
-alert("hola esto es una alerta")
+
+var barco;
+var barco_movent;
+var rio;
+
+
+function preload(){
+ barco_movent = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+
+
+}
+
+function setup(){
+  createCanvas(600,400);
+  barco = createSprite(100,200,20,20);
+  barco.addAnimation("movimiento",barco_movent);
+  
+}
+
+function draw() {
+  background("blue");
+  
+
+
+  drawSprite(); 
+}
